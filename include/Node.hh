bad48bdbc049e7bec9362092997cd0936719435e
unknown <marisela.delgadillo.cbtis37@gmail.com>
2020-09-08 16:42:55 -0700

    pointers

---
+++ include/Node.hh
@@ -0,0 +1,12 @@
+struct Node
+{
+    int value{};
+    Node* nextNode{nullptr};
+
+    Node(int value, Node* nextNode)
+    {
+        this->value = value;
+        this->nextNode = nextNode;
+    }
+
+};
